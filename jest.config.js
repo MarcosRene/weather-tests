0923db0d13f5f71a8264975a5aaa55408fccd7df
Marcos Rene <renemoura789@gmail.com>
2024-04-14 16:06:53 -0300

    refactor: async storage mock config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
 
 const config = {
   preset: "jest-expo",
-  setupFiles: ["<rootDir>/.jest/setup.ts"],
+  setupFiles: ["<rootDir>/__tests__/mocks/libs/async-storage.ts"],
   setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
