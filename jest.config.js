d5f57df7da0feeba072515647ff820b262dadb0d
Marcos Rene <renemoura789@gmail.com>
2024-04-15 12:59:56 -0300

    test: routes when has city selected

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
 
 const config = {
   preset: "jest-expo",
-  setupFiles: ["<rootDir>/__tests__/mocks/libs/async-storage.ts"],
+  setupFiles: ["<rootDir>/__tests__/mocks/libs/async-storage.ts", "<rootDir>/__tests__/mocks/libs/react-native-safe-area-context.ts"],
   setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
