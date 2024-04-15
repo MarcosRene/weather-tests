5b3d4b86e05645965527273ca3a207db187d4e66
Marcos Rene <renemoura789@gmail.com>
2024-04-15 14:26:30 -0300

    feat: coverage/test ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,32 @@
-
 /** @type {import('jest').Config} */
 
 const config = {
-  preset: "jest-expo",
-  setupFiles: ["<rootDir>/__tests__/mocks/libs/async-storage.ts", "<rootDir>/__tests__/mocks/libs/react-native-safe-area-context.ts"],
-  setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"],
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  testPathIgnorePatterns: [
+    './__tests__/mocks',
+    './__tests__/utils',
+    './__tests__/coverage',
+  ],
+  setupFiles: [
+    './__tests__/mocks/libs/async-storage.ts',
+    './__tests__/mocks/libs/react-native-safe-area-context.ts',
   ],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.svg$": "jest-transformer-svg"
-  }
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}', '!./src/**/styles.ts'],
+  coveragePathIgnorePatterns: [
+    './src/@types',
+    './src/styles',
+    './src/libs/dayjs',
+    './__tests__coverage',
+  ],
+  coverageDirectory: './__tests__/coverage',
 };
 
 module.exports = config;
